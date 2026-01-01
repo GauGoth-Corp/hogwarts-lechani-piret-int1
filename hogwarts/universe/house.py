27487d895a11d2ff9b3f7f67bea4d4d230ba4300
GauGoth-Corp <ggaugoth@gmail.com>
2026-01-02 00:43:56 +0100

    === FINAL SUBMISSION === Hogwarts Game v. 0.1

--- hogwarts/universe/house.py
+++ hogwarts/universe/house.py
@@ -3,7 +3,7 @@
 #### Hogwarts Game - v. 0.1 #################################
 ##### HOUSE ###################################################
 #### 27/11/2025 - 02/01/2026 #################################
-#### Copyright (c) 2025 GauGoth Corp. All Rights reserved ####
+#### Copyright (c) 2025-2026 GauGoth Corp. All Rights reserved ####
 ##############################################################
 """
 Functions for managing Hogwarts houses, including player distribution, updating points, and displaying the winning house.
