26c5338b74d7d1114bbe3f6b2f40cbe634098056
GauGoth-Corp <ggaugoth@gmail.com>
2026-01-06 17:35:38 +0100

    Refactor: CH4 ready to debuug

--- hogwarts/__debug__.py
+++ hogwarts/__debug__.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     
 
     igor_character = {
-                        "Last Name": "clair", 
-                        "First Name": "obscur", 
+                        "Last Name": "D'Osgor", 
+                        "First Name": "Igor", 
                         "Money": 531,
                         "Inventory": ["Magic Wand", "Bus full of children", "House elf", "Potions Book", "Wizard Robe"],
                         "Spells": ["Lumos", "Alohomora", "Expelliarmus", "Stupefy", "Rictusempra"], #Avada Kedavra
@@ -40,4 +40,5 @@ if __name__ == "__main__":
     dragonFightFirstRound(igor_character, hungarian_horntail)
     """
     
-    startChapter3(igor_character)
+    #startChapter3(igor_character)
+    startChapter4(igor_character)
